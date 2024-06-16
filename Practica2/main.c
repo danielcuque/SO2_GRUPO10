71089e8e0ea25539b2519c0084b4352ecb19166c
damianpeaf <damianpeaf@gmail.com>
2024-06-16 00:38:11 -0600

    chore: transactions

--- Practica2/main.c
+++ Practica2/main.c
@@ -6,7 +6,8 @@
 #include "cJSON.h"
 #include <unistd.h> // Para la función sleep
 
-#define NUM_THREADS 4
+#define NUM_THREADS 3
+#define NUM_THREADS_2 4
 
 typedef struct {
     int no_cuenta;
@@ -20,13 +21,21 @@ typedef struct {
     int initial_index;
     int final_index;
     int processed_records;
-} ThreadArgs;
+} UserThreadArgs;
+
+typedef struct {
+    cJSON *json;
+    int retiros;
+    int depositos;
+    int transferencias;
+    int operaciones;
+} TransactionThreadArgs;
 
 
 Cuenta *cuentas = NULL;
 int num_cuentas = 0;
 pthread_mutex_t cuentas_mutex = PTHREAD_MUTEX_INITIALIZER;
-
+int current_transaction = 0;
 
 void inicializar_cuenta(Cuenta *cuenta, int num, const char *nombre, double saldo) {
     cuenta->no_cuenta = num;
@@ -48,14 +57,11 @@ void agregar_cuenta(int num, const char *nombre, double saldo) {
 }
 
 int existe_cuenta(int num) {
-    pthread_mutex_lock(&cuentas_mutex);
     for (int i = 0; i < num_cuentas; i++) {
         if (cuentas[i].no_cuenta == num) {
-            pthread_mutex_unlock(&cuentas_mutex);
             return 1;
         }
     }
-    pthread_mutex_unlock(&cuentas_mutex);
     return 0;
 }
 
@@ -81,7 +87,7 @@ void generar_archivo(const char *filename, const char *content) {
 }
 
 
-void reporte_usuarios(ThreadArgs *thread_args) {
+void reporte_usuarios(UserThreadArgs *thread_args) {
     char nombre_archivo[100];
 
     char *fecha = obtener_fecha_formateada();
@@ -110,31 +116,46 @@ void reporte_usuarios(ThreadArgs *thread_args) {
     // free(content);
 }
 
-void reporte_operaciones() {
+void reporte_transacciones(TransactionThreadArgs *thread_args) {
     char nombre_archivo[100];
 
     char *fecha = obtener_fecha_formateada();
     snprintf(nombre_archivo, sizeof(nombre_archivo),
              "operaciones_%s.log", fecha);
 
-    int retiros = 0;
-    int depositos = 0;
-    int transferencias = 0;
-    int total = retiros + depositos + transferencias;
+
+    int total_depositos = 0;
+    int total_retiros = 0;
+    int total_transferencias = 0;
+
+    for (int i = 0; i < NUM_THREADS_2; i++) {
+        total_depositos += thread_args[i].depositos;
+        total_retiros += thread_args[i].retiros;
+        total_transferencias += thread_args[i].transferencias;
+    }
+
+    int total = total_depositos + total_retiros + total_transferencias;
+
     char body[1024];
     snprintf(body, sizeof(body), "---------- Resumen de operaciones ----------\n");
     snprintf(body + strlen(body), sizeof(body) - strlen(body), "Fecha: %s\n\n", fecha);
-    snprintf(body + strlen(body), sizeof(body) - strlen(body), "Operaciones realizadas:"
-             "Retiros: %d\n"
+    snprintf(body + strlen(body), sizeof(body) - strlen(body), "Operaciones realizadas:\n"
              "Depositos: %d\n"
+             "Retiros: %d\n"
              "Transferencias: %d\n"
              "Total: %d\n\n",
-             retiros, depositos, transferencias, total);
+                total_depositos, total_retiros, total_transferencias, total);
+    snprintf(body + strlen(body), sizeof(body) - strlen(body), "Operaciones por hilo:\n\n");
+    for (int i = 0; i < NUM_THREADS_2; i++) {
+        snprintf(body + strlen(body), sizeof(body) - strlen(body), "Hilo %d: %d\n", i, thread_args[i].depositos + thread_args[i].retiros + thread_args[i].transferencias);
+    }
+    snprintf(body + strlen(body), sizeof(body) - strlen(body), "\nErrores:\n\n");   
     generar_archivo(nombre_archivo, body);
 }
 
+
 void *cargar_usuarios_thread(void *args) {
-    ThreadArgs *thread_args = (ThreadArgs *)args;
+    UserThreadArgs *thread_args = (UserThreadArgs *)args;
     cJSON *json = thread_args->json;
     int initial_index = thread_args->initial_index;
     int final_index = thread_args->final_index;
@@ -204,7 +225,7 @@ void cargar_usuarios(const char *filename) {
 
     // Crear los hilos
     pthread_t threads[NUM_THREADS];
-    ThreadArgs thread_args[NUM_THREADS];
+    UserThreadArgs thread_args[NUM_THREADS];
     int i;
     for (i = 0; i < NUM_THREADS; i++) {
         thread_args[i].json = json;
@@ -283,6 +304,7 @@ int retiro_transaccion(int no_cuenta, double monto) {
 
             cuentas[i].saldo -= monto;
             pthread_mutex_unlock(&cuentas[i].mutex);
+            retiro_realizado = 1;
             break;
         }
     }
@@ -410,6 +432,226 @@ void mostrar_cuenta(int no_cuenta) {
     printf("La cuenta no existe.\n");
 }
 
+void *cargar_operaciones_masiva_thread(void *args) {
+    TransactionThreadArgs *thread_args = (TransactionThreadArgs *)args;
+    cJSON *json = thread_args->json;
+
+    while (1) {
+        pthread_mutex_lock(&cuentas_mutex);
+        if (current_transaction >= cJSON_GetArraySize(json)) {
+            pthread_mutex_unlock(&cuentas_mutex);
+            break;
+        }
+
+        cJSON *element = cJSON_GetArrayItem(json, current_transaction);
+        thread_args->operaciones++;
+        current_transaction++;
+        
+        if (!element) {
+            pthread_mutex_unlock(&cuentas_mutex);
+            continue;
+        }
+
+
+        // 1 -> Deposito; 2 -> Retiro; 3 -> Transferencia
+        cJSON *operacion = cJSON_GetObjectItem(element, "operacion");
+        cJSON *cuenta1 = cJSON_GetObjectItem(element, "cuenta1");
+        cJSON *cuenta2 = cJSON_GetObjectItem(element, "cuenta2");
+        cJSON *monto = cJSON_GetObjectItem(element, "monto");
+
+        if (!operacion) {
+            pthread_mutex_unlock(&cuentas_mutex);
+            continue;
+        }
+
+
+        // TODO: AGREGAR ERRORES
+        if (operacion->valueint == 1) {
+            // Validaciones de depósito
+            if (!cuenta1 || !monto) {
+                printf("Para deposito se necesita cuenta y monto\n");
+                pthread_mutex_unlock(&cuentas_mutex);
+                continue;
+            }
+            
+            // existencia de cuenta
+            if (!existe_cuenta(cuenta1->valueint)) {
+                fprintf(stderr, "La cuenta %d no existe.\n", cuenta1->valueint);
+                pthread_mutex_unlock(&cuentas_mutex);
+                continue;
+            }
+
+            // Validar el monto
+            if (monto->valuedouble <= 0) {
+                fprintf(stderr, "El monto debe ser mayor a cero.\n");
+                pthread_mutex_unlock(&cuentas_mutex);
+                continue;
+            }           
+
+            deposito_transaccion(cuenta1->valueint, monto->valuedouble);
+            fprintf(stderr, "Deposito realizado con exito\n");
+            thread_args->depositos++;
+        } else if (operacion->valueint == 2) {
+            // Validaciones de retiro
+            if (!cuenta1 || !monto) {
+                fprintf(stderr, "Para retiro se necesita cuenta y monto\n");
+                pthread_mutex_unlock(&cuentas_mutex);
+                continue;
+            }
+
+            // existencia de cuenta
+            if (!existe_cuenta(cuenta1->valueint)) {
+                fprintf(stderr, "La cuenta %d no existe.\n", cuenta1->valueint);
+                pthread_mutex_unlock(&cuentas_mutex);
+                continue;
+            }
+
+            // Validar el monto
+            if (monto->valuedouble <= 0) {
+                fprintf(stderr, "El monto debe ser mayor a cero.\n");
+                pthread_mutex_unlock(&cuentas_mutex);
+                continue;
+            }
+
+            if (!retiro_transaccion(cuenta1->valueint, monto->valuedouble)){
+                fprintf(stderr, "Saldo insuficiente\n");
+            } else {
+                fprintf(stderr, "Retiro realizado con exito\n");
+                thread_args->retiros++;
+            }
+        } else if (operacion->valueint == 3) {
+
+            // Validaciones de transferencia
+            if (!cuenta1 || !cuenta2 || !monto) {
+                fprintf(stderr, "Para transferencia se necesita cuenta1, cuenta2 y monto\n");
+                pthread_mutex_unlock(&cuentas_mutex);
+                continue;
+            }
+
+            // existencia de cuenta
+            if (!existe_cuenta(cuenta1->valueint) || !existe_cuenta(cuenta2->valueint)) {
+                fprintf(stderr, "La cuenta %d o %d no existe.\n", cuenta1->valueint, cuenta2->valueint);
+                pthread_mutex_unlock(&cuentas_mutex);
+                continue;
+            }
+
+            // Validar el monto
+            if (monto->valuedouble <= 0) {
+                fprintf(stderr, "El monto debe ser mayor a cero.\n");
+                pthread_mutex_unlock(&cuentas_mutex);
+                continue;
+            }
+
+            if (!transferencia_transaccion(cuenta1->valueint, cuenta2->valueint, monto->valuedouble)){
+                fprintf(stderr, "Saldo insuficiente\n");
+            } else {
+                fprintf(stderr, "Transferencia realizada con exito\n");
+                thread_args->transferencias++;
+            }
+        }
+
+        pthread_mutex_unlock(&cuentas_mutex);
+    }
+}
+
+void cargar_operaciones_masiva(){
+    char filename[100];
+    printf("Nombre del archivo de operaciones: ");
+    scanf("%s", filename);
+
+    // Abrir el archivo JSON
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "No se pudo abrir el archivo.\n");
+        return;
+    }
+
+    // Obtener el tamaño del archivo
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    // Leer el contenido del archivo
+    char *json_content = (char *)malloc(file_size + 1);
+    fread(json_content, 1, file_size, file);
+    json_content[file_size] = '\0';
+
+    // Cerrar el archivo
+    fclose(file);
+
+    // Analizar el JSON
+    cJSON *json = cJSON_Parse(json_content);
+
+    if (!json) {
+        fprintf(stderr, "Error al analizar el JSON.\n");
+        free(json_content);
+        return;
+    
+    }
+
+    // Check if it's a valid JSON array
+    if (!cJSON_IsArray(json)) {
+        perror("JSON is not Array");
+        return;
+    }
+
+    // Vamos a trabajar de forma concurrente con 3 hilos
+    pthread_t threads[NUM_THREADS_2];
+    TransactionThreadArgs thread_args[NUM_THREADS_2];
+    current_transaction = 0;
+
+    // Crear los hilos
+    for (int i = 0; i < NUM_THREADS_2; i++) {
+        thread_args[i].json = json;
+        thread_args[i].retiros = 0;
+        thread_args[i].depositos = 0;
+        thread_args[i].transferencias = 0;
+        thread_args[i].operaciones = 0;
+        pthread_create(&threads[i], NULL, cargar_operaciones_masiva_thread, &thread_args[i]);
+    }
+
+    // Esperar a que los hilos terminen
+    for (int i = 0; i < NUM_THREADS_2; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    reporte_transacciones(thread_args);
+
+    // Liberar la memoria y destruir el semáforo
+    cJSON_Delete(json);
+    free(json_content);
+}
+
+void generar_reporte_json(const char *filename, Cuenta *cuentas, int num_cuentas) {
+    FILE *file = fopen(filename, "w");
+    if (!file) {
+        fprintf(stderr, "No se pudo abrir el archivo %s.\n", filename);
+        return;
+    }
+
+    // Comenzar a escribir el array JSON
+    fprintf(file, "[\n");
+
+    for (int i = 0; i < num_cuentas; i++) {
+        fprintf(file, "  {\n");
+        fprintf(file, "    \"no_cuenta\": %d,\n", cuentas[i].no_cuenta);
+        fprintf(file, "    \"nombre\": \"%s\",\n", cuentas[i].nombre);
+        fprintf(file, "    \"saldo\": %.2f\n", cuentas[i].saldo);
+
+        // Coma después de cada objeto, excepto el último
+        if (i < num_cuentas - 1) {
+            fprintf(file, "  },\n");
+        } else {
+            fprintf(file, "  }\n");
+        }
+    }
+
+    // Finalizar el array JSON
+    fprintf(file, "]\n");
+
+    fclose(file);
+}
+
 
 
 void menu() {
@@ -449,8 +691,11 @@ void menu() {
                 mostrar_cuenta(no_cuenta);
                 break;
             case 7:
+                cargar_operaciones_masiva();
                 break;
             case 8:
+                // Generar el reporte en formato JSON
+                generar_reporte_json("cuentas.json", cuentas, num_cuentas);
                 break;
             case 9:
                 break;
